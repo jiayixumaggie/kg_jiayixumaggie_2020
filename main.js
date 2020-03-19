5fa669a3295e397e7822d2af0c5e83cb4033b3cc
Jiayi Xu <jx347@cornell.edu>
2020-03-19 15:04:02 -0400

    initial solution char_map function complete

---
+++ main.js
@@ -0,0 +1,20 @@
+const char_map = function(s1, s2) {
+  let map = new Map();
+
+  // s1 must have the same length as s2
+  if (s1.length !== s2.length) {
+    return false;
+  }
+
+  // iterate s1 to check mapping
+  for (let i = 0; i < s1.length; i++) {
+    if (!map.has(s1[i])) {
+      map.set(s1[i], s2[i]);
+    } else {
+      if (map.get(s1[i]) !== s2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
